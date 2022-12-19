0f9367603f2d2abcf47ccee96ebd485bf41bfa17
Aleksey <aliaksei.balabushka@gmail.com>
2022-12-19 11:20:51 +0300

    feat: complete logger task

--- src/08-functions-n-closures-tasks.js
+++ src/08-functions-n-closures-tasks.js
@@ -144,8 +144,13 @@ function retry(/* func, attempts */) {
  * cos(3.141592653589793) ends
  *
  */
-function logger(/* func, logFunc */) {
-  throw new Error('Not implemented');
+function logger(func, logFunc) {
+  return (...args) => {
+    const value = `${func.name}(${JSON.stringify(...args)})`;
+    logFunc(`${value} starts`);
+    logFunc(`${value} ends`);
+    return func(...args);
+  };
 }
 
 
